b68dc5fbe20e34a680f3a966de770c2ca6ab56d6
Masaya <macbeth0129will@gmail.com>
2025-07-21 08:03:58 +0900

    レシピ詳細ページからLinkを削除

--- app/dashboard/recipe/[id]/page.tsx
+++ app/dashboard/recipe/[id]/page.tsx
@@ -2,7 +2,6 @@ import RecipeInfo from '@/app/ui/recipe/recipe-info';
 import Tag from '@/app/ui/recipe/tag';
 import Ingredients from '@/app/ui/recipe/ingredients';
 import Step from '@/app/ui/recipe/step';
-import Link from 'next/link';
 import EditBtn from '@/app/ui/recipe/edit-btn';
 import { fetchRecipe } from '@/app/lib/actions/recipe-actions';
 
@@ -12,13 +11,6 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 
   return (
     <main className="w-full flex flex-col gap-8">
-      {/* <Link href={`${id}/edit`} className="flex justify-end">
-        <EditBtn
-          color="bg-[#1F4529] text-[#E8ECD7] shadow-[0_3px_0_#32633f] hover:bg-[#32633f] 
-            active:bg-[#32633f] active:shadow-[0_3px_0_#32633f]"
-          size="w-4 h-4"
-        />
-      </Link> */}
       <EditBtn recipeData={recipeData} />
       <RecipeInfo recipeData={recipeData} />
       <Tag recipeData={recipeData} />
