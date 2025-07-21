9e30d16743c1f6494bc85cd7a1e9337402b76f4b
Masaya <macbeth0129will@gmail.com>
2025-07-21 22:15:09 +0900

    ナビゲーションバーの動作を変更

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -9,6 +9,7 @@ import ForkKnife from '@/app/ui/icons/fork-knife';
 import Home from '@/app/ui/icons/home';
 import BoxArrowRight from '@/app/ui/icons/box-arrow-right';
 import Calendar from '@/app/ui/icons/calendar';
+import CancelBtn from '@/app/ui/icons/cancel-btn';
 
 const links: Links[] = [
   { name: 'ホーム', href: '/dashboard', icon: Home, width: 'w-6' },
@@ -30,29 +31,43 @@ const links: Links[] = [
 
 export default function Nav(props: { isOpen: boolean; openNav?: () => void }) {
   return (
-    <nav
-      className={`w-full h-screen bg-[#1F4529] text-[#E8ECD7] rounded-2xl 
-        px-6 py-8 transition-all duration-500 ease-in-out`}
-    >
-      {links.map((link) => (
-        <div key={link.name}>
-          <Link
-            href={link.href}
-            onClick={props.openNav}
-            className="flex gap-4 py-5 items-center"
-          >
-            <link.icon width={link.width} />
-            <p className="font-bold">{link.name}</p>
-          </Link>
-          <hr />
-        </div>
-      ))}
-      <form action={logout} className="">
-        <button className="flex gap-4 py-5 items-center">
-          <BoxArrowRight width="w-6" />
-          <div className="font-bold">ログアウト</div>
+    <>
+      <div
+        className={`fixed w-full h-full bg-[rgba(0,0,0,.5)] 
+              top-0 left-0 z-1000`}
+      ></div>
+      <nav
+        className={`fixed top-0 left-0 z-1100 w-75 h-screen bg-[#1F4529] 
+        text-[#E8ECD7] rounded-r-2xl 
+        px-8 py-10 transition-all duration-500 ease-in-out`}
+      >
+        <button
+          type="button"
+          className="ml-auto block mb-4"
+          onClick={props.openNav}
+        >
+          <CancelBtn fillColor={'#E8ECD7'} cN={'w-5 block ml-auto'} />
         </button>
-      </form>
-    </nav>
+        {links.map((link) => (
+          <div key={link.name}>
+            <Link
+              href={link.href}
+              onClick={props.openNav}
+              className="flex gap-4 py-5 items-center"
+            >
+              <link.icon width={link.width} />
+              <p className="font-bold">{link.name}</p>
+            </Link>
+            <hr />
+          </div>
+        ))}
+        <form action={logout} className="">
+          <button className="flex gap-4 py-5 items-center">
+            <BoxArrowRight width="w-6" />
+            <div className="font-bold">ログアウト</div>
+          </button>
+        </form>
+      </nav>
+    </>
   );
 }
