8e1f2b31ded4065df931def70347d9c8b2ca033e
Masaya <macbeth0129will@gmail.com>
2025-07-21 10:35:42 +0900

    型を整理

--- app/dashboard/set-meal/[id]/edit/page.tsx
+++ app/dashboard/set-meal/[id]/edit/page.tsx
@@ -5,12 +5,12 @@ import { fetchRecipes } from '@/app/lib/actions/recipe-actions';
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const { id } = await props.params;
   const setMeal = await fetchSetMeal(id);
-  const allRecipes = await fetchRecipes();
+  const recipes = await fetchRecipes();
 
   return (
     <main className="w-full py-5 flex flex-col gap-4">
       <h1 className="font-bold text-2xl">献立を編集</h1>
-      {setMeal && <SetMealEditForm setMeal={setMeal} allRecipes={allRecipes} />}
+      {setMeal && <SetMealEditForm setMeal={setMeal} recipes={recipes} />}
     </main>
   );
 }
