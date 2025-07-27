c5f06ef0467c251e6f7b4d9094638e9b99f5ecdc
Masaya <macbeth0129will@gmail.com>
2025-07-27 09:44:59 +0900

    ナビゲーションバーの幅を変更

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,8 +3,6 @@ import Footer from '@/app/ui/footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    // <div className="p-5 flex flex-col gap-2 max-w-[1120px] mx-auto my-0 min-h-screen">
-    // <div className="px-15 py-5 flex flex-col gap-2 mx-auto my-0 min-h-screen">
     <div className="p-5 flex flex-col gap-2 max-w-[1120px] mx-auto my-0 min-h-screen">
       <Main>{children}</Main>
       <Footer />
--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -10,6 +10,7 @@ import Home from '@/app/ui/icons/home';
 import BoxArrowRight from '@/app/ui/icons/box-arrow-right';
 import Calendar from '@/app/ui/icons/calendar';
 import CancelBtn from '@/app/ui/icons/cancel-btn';
+import Image from 'next/image';
 
 const links: Links[] = [
   { name: 'ホーム', href: '/dashboard', icon: Home, width: 'w-6' },
@@ -38,16 +39,26 @@ export default function Nav(props: { isOpen: boolean; openNav?: () => void }) {
       ></div>
       <nav
         className={`fixed top-0 left-0 z-1100 w-75 h-screen bg-[#1F4529] 
-        text-[#E8ECD7] rounded-r-2xl 
-        px-8 py-10 transition-all duration-500 ease-in-out md:w-100`}
+          text-[#E8ECD7] rounded-r-2xl 
+          px-8 py-10 transition-all duration-100 ease-in-out md:w-100`}
       >
-        <button
-          type="button"
-          className="ml-auto block mb-4"
-          onClick={props.openNav}
-        >
-          <CancelBtn fillColor={'#E8ECD7'} cN={'w-5 block ml-auto'} />
-        </button>
+        <div className="flex justify-between items-center mb-10">
+          <Image
+            src="/logo_reverse_4.png"
+            width={228}
+            height={144}
+            alt="logo"
+            className="w-15 md:w-20"
+          />
+          <button
+            type="button"
+            className="ml-auto block"
+            onClick={props.openNav}
+          >
+            <CancelBtn fillColor={'#E8ECD7'} cN={'w-5 md:w-8 block ml-auto'} />
+          </button>
+        </div>
+
         {links.map((link) => (
           <div key={link.name}>
             <Link
